e209caa2cf2d9c3acfa2ae3ac51986eef1f4ffb8
vgglv <ripykt@gmail.com>
2025-06-02 22:29:15 +0900

    wip?

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,16 @@
-import
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
+data class User(val name: String, val age: Int)
 
 fun main() {
+    val jsonString = """
+        {
+            "name": "Baska",
+            "age": 30
+        }
+    """.trimIndent()
+    val user = Json.decodeFromString<User>(jsonString)
+    println(user)
 }
